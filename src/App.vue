a4b9b227f06a33656f224420e5582056488b6535
Emmanuel <emmlop243@gmail.com>
2021-09-29 17:45:49 -0500

    TailwindCss got installed. The indecision.vue file has been styled using tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
 </template>
 
 <script setup>
-import Indecision from "./components/Indecision.vue";
+import Indecision from './components/Indecision.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
